c6a677ec6998bcbff953eb2e6db222f87da6b327
Andrey Zhulay <zhulay.00@mail.ru>
2023-04-10 21:08:00 +0300

    первая фиксация

---
+++ Task66/66.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу,
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int m, n;
+Console.Write($"Введите М: ");
+int.TryParse(Console.ReadLine(), out m);
+Console.Write($"Введите N: ");
+int.TryParse(Console.ReadLine(), out n);
+
+int sum= 0;
+
+Console.Write(SumNumbers(m, n, sum));
+
+string SumNumbers(int m, int n, int sum)
+{
+    if (m > n)
+        return $"{sum}";
+         sum = sum + m ++ ;
+            return $"{sum}, -> {SumNumbers( m, n, sum)}";
+           
+}
---
+++ Task68/68.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 68: Напишите программу вычисления функции Аккермана 
+с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+int m;
+int n;
+Console.Write($"Введите М: ");
+int.TryParse(Console.ReadLine(), out m);
+Console.Write($"Введите N: ");
+int.TryParse(Console.ReadLine(), out n);
+
+Console.Write(AckermanF(m, n));
+
+int AckermanF(int m,int  n)
+{
+    if (m == 0)
+         return n + 1;
+     if (m != 0 && n == 0 )
+     return AckermanF(m - 1, 1);
+     if (m > 0 && n > 0)
+     return AckermanF(m - 1,AckermanF(m, n-1));
+   return AckermanF( m, n);
+}
+
+Console.WriteLine($" Функуия Акермана для чисел {m}, {n} = {AckermanF(m, n)}");
